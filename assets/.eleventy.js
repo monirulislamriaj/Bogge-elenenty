fc0df274cf909076c60a42050fe71ba6144a1ecd
monirulislamriaj <riaj1132@gmail.com>
2021-02-22 19:05:01 +0600

    initil commit

---
+++ assets/.eleventy.js
@@ -0,0 +1,16 @@
+// eleventy configuration
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+
+  return {
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "md"],
+    dir: {
+      input: "src",
+      output: "_site",
+      include: "_includes",
+    },
+  };
+};
